d787dc058396f848a3a65700b4e0c5df4fb3424f
Arie2238 <arieschouten2238@hotmail.com>
2017-07-08 00:52:27 +0200

    Search Controller start gemaakt.
    Search controller start gemaakt.

---
+++ Controller/AdminSearchController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Arie Schouten
+ * Date: 28-6-2017
+ * Time: 20:16
+ */
+
+namespace Controller;
+
+
+class AdminSearchController
+{
+
+}
---
+++ Controller/SearchController.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Arie Schouten
+ * Date: 28-6-2017
+ * Time: 20:17
+ */
+
+namespace Controller;
+
+
+use Main\Controller;
+use Model\Product\ProductManager;
+use Model\Translation\SystemTranslation;
+
+class SearchController extends Controller
+{
+    public function searchAction($search=null)
+    {
+        if (empty($_POST['search'])) {
+            return $this->write(array('error'=> 'dit is een test'));
+        }
+        $search = $_POST['search'];
+        $productmanager = new ProductManager($this->getLanguage());
+        $systemTranslation = new SystemTranslation($this->getLanguage());
+        $products = $productmanager->getProducts($search);
+
+
+        if (!$products) {
+            return $this->write(array('error' => 'Zoek opdracht niet gevonden'));
+        }
+
+        $values = array('products'=>$products,
+                        'systemTranslation'=>$systemTranslation);
+
+        return $this->write($values);
+    }
+
+}
